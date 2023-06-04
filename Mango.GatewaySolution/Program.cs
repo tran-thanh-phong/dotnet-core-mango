d2a977cd6cfa585af26afce2969ec4dd5326eea1
Tran Thanh Phong <tranthanhphong91@gmail.com>
2023-06-04 11:31:49 +0700

    Section 18 - Gateway Project - Ocelot

---
+++ Mango.GatewaySolution/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options => {
+        options.Authority = "https://localhost:7228/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.Run();
